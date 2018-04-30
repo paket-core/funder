6a7402a7789ed02b46e4bd44323a0ddcec1d46df
Israel Levin <israel@paket.global>
2018-04-30 21:55:08 +0300

    Setup webserver.

--- identity.py
+++ identity.py
@@ -13,20 +13,17 @@ LOGGER = logger.logging.getLogger('pkt.identity')
 
 VERSION = swagger_spec.CONFIG['info']['version']
 BLUEPRINT = flask.Blueprint('identity', __name__)
-webserver.APP.config['SWAGGER'] = swagger_spec.CONFIG
-flasgger.Swagger(webserver.APP)
-webserver.APP.register_blueprint(BLUEPRINT)
 
 # pylint: disable=missing-docstring
 # See documentation in swagger_spec.
 
 
 @BLUEPRINT.route("/v{}/test".format(VERSION), methods=['GET'])
-@flasgger.swag_from(swagger_spec.TEST)
-@webserver.call
+#@flasgger.swag_from(swagger_spec.TEST)
+#@webserver.call
 def test_handler():
     return {'status': 200, 'message': 'This is indeed a test.'}
 
 
 if __name__ == '__main__':
-    webserver.APP.run()
+    webserver.run(BLUEPRINT, swagger_spec.CONFIG, True)
