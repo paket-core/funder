3cd11e1f1d971c527eade04d7f80783e9f5fe63f
Israel Levin <israel@paket.global>
2018-05-01 17:59:41 +0300

    Basic functionality.

--- identity.py
+++ identity.py
@@ -7,24 +7,58 @@ import logger
 import webserver
 import webserver.validation
 
-import swagger_spec
+import db
+import swagger_specs
 
 logger.setup()
 LOGGER = logger.logging.getLogger('pkt.identity')
 
-VERSION = swagger_spec.CONFIG['info']['version']
+VERSION = swagger_specs.CONFIG['info']['version']
 BLUEPRINT = flask.Blueprint('identity', __name__)
 
-# pylint: disable=missing-docstring
-# See documentation in swagger_spec.
+# This should be removed soon.
+db.init_db()
 
 
-@BLUEPRINT.route("/v{}/test".format(VERSION), methods=['GET'])
-@flasgger.swag_from(swagger_spec.TEST)
-@webserver.validation.call
-def test_handler():
-    return {'status': 200, 'message': 'This is indeed a test.'}
+@BLUEPRINT.route("/v{}/user".format(VERSION), methods=['POST'])
+@flasgger.swag_from(swagger_specs.USER_POST)
+@webserver.validation.call(['full_name', 'phone_number', 'paket_user'], require_auth=True)
+def user_post_handler(user_pubkey, full_name, phone_number, paket_user):
+    """Add user details."""
+    db.create_user(user_pubkey, full_name, phone_number, paket_user)
+    return {'status': 201, 'user': db.get_user(user_pubkey)}
+
+
+@BLUEPRINT.route("/v{}/user".format(VERSION), methods=['GET'])
+@flasgger.swag_from(swagger_specs.USER_GET)
+@webserver.validation.call(require_auth=True)
+def user_get_handler(user_pubkey, queried_pubkey=None):
+    """Get user details."""
+    if queried_pubkey is None:
+        queried_pubkey = user_pubkey
+    elif queried_pubkey != user_pubkey:
+        assert db.is_authorized(user_pubkey, queried_pubkey), "pubkey {} is not authorized to view {}".format(
+            user_pubkey, queried_pubkey)
+    return {'status': 200, 'user': db.get_user(queried_pubkey)}
+
+
+@BLUEPRINT.route("/v{}/authorize".format(VERSION), methods=['POST'])
+@flasgger.swag_from(swagger_specs.AUTHORIZE)
+@webserver.validation.call(['authorized_pubkey'], require_auth=True)
+def authorize_handler(user_pubkey, authorized_pubkey):
+    """Authorize a pubkey to view user_pubkey's details."""
+    db.add_authorization(authorized_pubkey, user_pubkey)
+    return {'status': 201}
+
+
+@BLUEPRINT.route("/v{}/unauthorize".format(VERSION), methods=['POST'])
+@flasgger.swag_from(swagger_specs.UNAUTHORIZE)
+@webserver.validation.call(['authorized_pubkey'], require_auth=True)
+def unauthorize_handler(user_pubkey, authorized_pubkey):
+    """Unauthorize a pubkey to view user_pubkey's details."""
+    db.remove_authorization(authorized_pubkey, user_pubkey)
+    return {'status': 201}
 
 
 if __name__ == '__main__':
-    webserver.run(BLUEPRINT, swagger_spec.CONFIG)
+    webserver.run(BLUEPRINT, swagger_specs.CONFIG)
---
+++ swagger_specs.py
@@ -0,0 +1,226 @@
+"""Swagger specifications of Identity Server."""
+VERSION = 1
+CONFIG = {
+    'title': 'PaKeT identity API',
+    'uiversion': 2,
+    'specs_route': '/',
+    'specs': [{
+        'endpoint': '/',
+        'route': '/apispec.json',
+    }],
+    'info': {
+        'title': 'The PaKeT Identity Server API',
+        'version': VERSION,
+        'contact': {
+            'name': 'The PaKeT Project',
+            'email': 'israel@paket.global',
+            'url': 'https://identity.paket.global',
+        },
+        'license': {
+            'name': 'GNU GPL 3.0',
+            'url': 'http://www.gnu.org/licenses/'
+        },
+        'description': '''
+What does this do?
+
+The API
+=======
+        '''
+    }
+}
+
+USER_POST = {
+    'parameters': [
+        {
+            'name': 'Pubkey',
+            'default': 'GBQOQ4LJC5YNIAYIC3WPNGLPHNBKAP6UJTLC3KGXI6QLZSFGJSASEOC4',
+            'in': 'header',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'Fingerprint',
+            'in': 'header',
+            'default':
+                'NOT NEEDED YET http://localhost:5000/v1/send_buls,to_pubkey=pubkey,amount_buls=amount,1521650747',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'Signature',
+            'in': 'header',
+            'default': 'NOT NEEDED YET 0xa7d77cf679a2456325bbba3b92d994f5987b68c147bad18e24e6b66f5dc',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'full_name',
+            'in': 'formData',
+            'type': 'string',
+            'required': True,
+        },
+        {
+            'name': 'phone_number',
+            'in': 'formData',
+            'type': 'string',
+            'required': True,
+        },
+        {
+            'name': 'paket_user',
+            'in': 'formData',
+            'type': 'string',
+            'required': True,
+        },
+    ],
+    'responses': {
+        '201': {
+            'description': 'User created',
+            'examples': {
+            }
+        }
+    }
+}
+
+USER_GET = {
+    'parameters': [
+        {
+            'name': 'Pubkey',
+            'default': 'GBQOQ4LJC5YNIAYIC3WPNGLPHNBKAP6UJTLC3KGXI6QLZSFGJSASEOC4',
+            'in': 'header',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'Fingerprint',
+            'in': 'header',
+            'default':
+                'NOT NEEDED YET http://localhost:5000/v1/send_buls,to_pubkey=pubkey,amount_buls=amount,1521650747',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'Signature',
+            'in': 'header',
+            'default': 'NOT NEEDED YET 0xa7d77cf679a2456325bbba3b92d994f5987b68c147bad18e24e6b66f5dc',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'queried_pubkey',
+            'in': 'query',
+            'type': 'string',
+            'required': False,
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': 'User retrieved',
+            'examples': {
+            }
+        }
+    }
+}
+
+AUTHORIZE = {
+    'parameters': [
+        {
+            'name': 'Pubkey',
+            'default': 'GBQOQ4LJC5YNIAYIC3WPNGLPHNBKAP6UJTLC3KGXI6QLZSFGJSASEOC4',
+            'in': 'header',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'Fingerprint',
+            'in': 'header',
+            'default':
+                'NOT NEEDED YET http://localhost:5000/v1/send_buls,to_pubkey=pubkey,amount_buls=amount,1521650747',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'Signature',
+            'in': 'header',
+            'default': 'NOT NEEDED YET 0xa7d77cf679a2456325bbba3b92d994f5987b68c147bad18e24e6b66f5dc',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'authorized_pubkey',
+            'in': 'formData',
+            'type': 'string',
+            'required': True,
+        },
+    ],
+    'responses': {
+        '201': {
+            'description': 'Authorization added',
+            'examples': {
+            }
+        }
+    }
+}
+
+UNAUTHORIZE = {
+    'parameters': [
+        {
+            'name': 'Pubkey',
+            'default': 'GBQOQ4LJC5YNIAYIC3WPNGLPHNBKAP6UJTLC3KGXI6QLZSFGJSASEOC4',
+            'in': 'header',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'Fingerprint',
+            'in': 'header',
+            'default':
+                'NOT NEEDED YET http://localhost:5000/v1/send_buls,to_pubkey=pubkey,amount_buls=amount,1521650747',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'Signature',
+            'in': 'header',
+            'default': 'NOT NEEDED YET 0xa7d77cf679a2456325bbba3b92d994f5987b68c147bad18e24e6b66f5dc',
+            'schema': {
+                'type': 'string',
+                'format': 'string'
+            }
+        },
+        {
+            'name': 'authorized_pubkey',
+            'in': 'formData',
+            'type': 'string',
+            'required': True,
+        },
+    ],
+    'responses': {
+        '201': {
+            'description': 'Authorization revoked',
+            'examples': {
+            }
+        }
+    }
+}
