952438abbdd4d3e2dd8f93860510be8a7e72f66f
Yaroslav R <yaroslav.r@it-dimension.com>
2018-07-03 11:01:43 +0300

    Move db access from routines_test module.

--- tests/routines_test.py
+++ tests/routines_test.py
@@ -6,7 +6,6 @@ import util.logger
 import db
 import routines
 
-
 LOGGER = util.logger.logging.getLogger('pkt.funder.test')
 
 
@@ -30,48 +29,22 @@ class RoutinesTest(unittest.TestCase):
         db.create_user('GBD5666CDBM6MS3RKXLO5WOXVJXECOCLBAE6B62XMNKZLF63GC6V3IB5', 'callsign3')
         db.create_user('GC2QTLYQXYFEOFQ26QNEDZR5VQVHLQPCXA5S4JU64OMW5IXFF5J4L52Z', 'callsign4')
         db.create_user('GDZYRJQTZ7LG2MIJJ35MTY55D7MTM7RV533KNBGXSU47Q5DMGLDXONBR', 'callsign5')
-        with db.SQL_CONNECTION() as sql:
-            sql.execute(
-                """INSERT INTO test_results (pubkey, name, result)
-                VALUES ('GAPAVB6IW4UNQTP4XFSRF4L6PS2XZD22IG6Z6FV6FXGZV7T3VL4TOAYQ', 'basic', '1')""")
-            sql.execute(
-                """INSERT INTO test_results (pubkey, name, result)
-                VALUES ('GBLZA2SZ3XJLCFYKW7TF6Q7FAMA4WJUOZN6J6WM3Z676W3JOJDQ6CEKE', 'basic', '0')""")
-            sql.execute(
-                """INSERT INTO test_results (pubkey, name, result)
-                VALUES ('GBD5666CDBM6MS3RKXLO5WOXVJXECOCLBAE6B62XMNKZLF63GC6V3IB5', 'basic', '1')""")
-            sql.execute(
-                """INSERT INTO test_results (pubkey, name, result)
-                VALUES ('GC2QTLYQXYFEOFQ26QNEDZR5VQVHLQPCXA5S4JU64OMW5IXFF5J4L52Z', 'basic', '0')""")
-            sql.execute(
-                """INSERT INTO test_results (pubkey, name, result)
-                VALUES ('GDZYRJQTZ7LG2MIJJ35MTY55D7MTM7RV533KNBGXSU47Q5DMGLDXONBR', 'basic', '1')""")
-        with db.SQL_CONNECTION() as sql:
-            sql.execute(
-                """INSERT INTO purchases (user_pubkey, payment_pubkey,
-                payment_currency, requested_currency, euro_cents, paid)
-                VALUES ('GAPAVB6IW4UNQTP4XFSRF4L6PS2XZD22IG6Z6FV6FXGZV7T3VL4TOAYQ',
-                '2N6WqqshyxoWuBGHLjbwnWAQSigJ4TJkYrt', 'BTC', 'BUL', '600', '1')""")
-            sql.execute(
-                """INSERT INTO purchases (user_pubkey, payment_pubkey,
-                payment_currency, requested_currency, euro_cents, paid)
-                VALUES ('GAPAVB6IW4UNQTP4XFSRF4L6PS2XZD22IG6Z6FV6FXGZV7T3VL4TOAYQ',
-                '0xBAEc12c49fea1f3AC84Fa9DC2A8A3559Fe81bC5F', 'ETH', 'BUL', '500', '0')""")
-            sql.execute(
-                """INSERT INTO purchases (user_pubkey, payment_pubkey,
-                payment_currency, requested_currency, euro_cents, paid)
-                VALUES ('GBD5666CDBM6MS3RKXLO5WOXVJXECOCLBAE6B62XMNKZLF63GC6V3IB5',
-                '0x2F41CCaD2466AdFe3A5C1Ed60b1589e125130ef3', 'ETH', 'BUL', '100', '0')""")
-            sql.execute(
-                """INSERT INTO purchases (user_pubkey, payment_pubkey,
-                payment_currency, requested_currency, euro_cents, paid)
-                VALUES ('GDZYRJQTZ7LG2MIJJ35MTY55D7MTM7RV533KNBGXSU47Q5DMGLDXONBR',
-                '0x3dcf887038BC4BD4710fEe6B49ecE233482B377F', 'ETH', 'BUL', '1200', '0')""")
-            sql.execute(
-                """INSERT INTO purchases (user_pubkey, payment_pubkey,
-                payment_currency, requested_currency, euro_cents, paid)
-                VALUES ('GDZYRJQTZ7LG2MIJJ35MTY55D7MTM7RV533KNBGXSU47Q5DMGLDXONBR',
-                '2NBMEXediyAYCGcVfW5W2toR5Kui1EpqaYB', 'BTC', 'XLM', '800', '0')""")
+        db.update_test('GAPAVB6IW4UNQTP4XFSRF4L6PS2XZD22IG6Z6FV6FXGZV7T3VL4TOAYQ', 'basic', 1)
+        db.update_test('GBLZA2SZ3XJLCFYKW7TF6Q7FAMA4WJUOZN6J6WM3Z676W3JOJDQ6CEKE', 'basic', 0)
+        db.update_test('GBD5666CDBM6MS3RKXLO5WOXVJXECOCLBAE6B62XMNKZLF63GC6V3IB5', 'basic', 1)
+        db.update_test('GC2QTLYQXYFEOFQ26QNEDZR5VQVHLQPCXA5S4JU64OMW5IXFF5J4L52Z', 'basic', 0)
+        db.update_test('GDZYRJQTZ7LG2MIJJ35MTY55D7MTM7RV533KNBGXSU47Q5DMGLDXONBR', 'basic', 1)
+        db.create_purchase('GAPAVB6IW4UNQTP4XFSRF4L6PS2XZD22IG6Z6FV6FXGZV7T3VL4TOAYQ',
+                           '2N6WqqshyxoWuBGHLjbwnWAQSigJ4TJkYrt', 'BTC', 'BUL', '600')
+        db.create_purchase('GAPAVB6IW4UNQTP4XFSRF4L6PS2XZD22IG6Z6FV6FXGZV7T3VL4TOAYQ',
+                           '0xBAEc12c49fea1f3AC84Fa9DC2A8A3559Fe81bC5F', 'ETH', 'BUL', '500')
+        db.create_purchase('GBD5666CDBM6MS3RKXLO5WOXVJXECOCLBAE6B62XMNKZLF63GC6V3IB5',
+                           '0x2F41CCaD2466AdFe3A5C1Ed60b1589e125130ef3', 'ETH', 'BUL', '100')
+        db.create_purchase('GDZYRJQTZ7LG2MIJJ35MTY55D7MTM7RV533KNBGXSU47Q5DMGLDXONBR',
+                           '0x3dcf887038BC4BD4710fEe6B49ecE233482B377F', 'ETH', 'BUL', '1200')
+        db.create_purchase('GDZYRJQTZ7LG2MIJJ35MTY55D7MTM7RV533KNBGXSU47Q5DMGLDXONBR',
+                           '2NBMEXediyAYCGcVfW5W2toR5Kui1EpqaYB', 'BTC', 'XLM', '800')
+        db.update_purchase('2N6WqqshyxoWuBGHLjbwnWAQSigJ4TJkYrt', 1)
 
     def test_check_purchases_addresses(self):
         """Test for check_purchases_addresses routine"""
