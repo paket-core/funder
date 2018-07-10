9ad7d7b3a91d1b34c1f55238bdb2da75c94b7f5c
Israel Levin <israel@paket.global>
2018-07-10 20:28:12 +0300

    Cosmetics.

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 """Run all tests."""
 # pylint: disable=wildcard-import,unused-wildcard-import
-from tests.csl_test import *
 from tests.db_test import *
-from tests.routines_test import *
+from tests.csl_test import *
 from tests.kyc_test import *
+from tests.routines_test import *
