f45596ded364f5ab6ef7b72e31ed58d7592c3a57
Yaroslav R <yaroslav.r@it-dimension.com>
2018-07-20 17:16:06 +0300

    (WIP) Added tests for creation user.

--- tests/routes_tests.py
+++ tests/routes_tests.py
@@ -2,9 +2,11 @@
 import json
 import unittest
 
+import paket_stellar
 import util.logger
 import webserver
 
+import db
 import routes
 
 LOGGER = util.logger.logging.getLogger('pkt.funder.test')
@@ -33,5 +35,33 @@ class BaseRoutesTests(unittest.TestCase):
         return response
 
 
-class RoutesTest(unittest.TestCase):
+class RoutesTest(BaseRoutesTests):
     """Test for routes"""
+
+    def test_create_user(self):
+        """Test create user"""
+        keypair = paket_stellar.get_keypair()
+        pubkey = keypair.address().decode()
+        call_sign = 'test_user'
+        user = self.call('create_user', 200, 'could not create user', user_pubkey=pubkey, call_sign=call_sign)
+        self.assertEqual(user['user_pubkey'], pubkey, '')
+        users = db.get_users()
+        self.assertEqual(len(users), 1, '')
+        self.assertEqual(user['user_pubkey'], users[0]['user_pubkey'], '')
+        self.assertEqual(user['call_sign'], users[0]['call_sign'])
+
+    def test_create_with_infos(self):
+        """Test create user with provided user info"""
+        keypair = paket_stellar.get_keypair()
+        pubkey = keypair.address().decode()
+        call_sign = 'test_user'
+        full_name = 'Kapitoshka Vodyanovych'
+        phone_number = '+380 67 13 666'
+        address = 'Vulychna 14, Trypillya'
+        user = self.call('create_user', 200, 'could not create user', user_pubkey=pubkey,
+                         call_sign=call_sign, full_name=full_name, phone_number=phone_number, address=address)
+        self.assertEqual(user['user_pubkey'], pubkey, '')
+        user_infos = db.get_user_infos(pubkey)
+        self.assertEqual(user_infos['full_name'], full_name, '')
+        self.assertEqual(user_infos['phone_number'], phone_number, '')
+        self.assertEqual(user_infos['address'], address, '')
