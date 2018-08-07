eefb862ea54899772883a80e18f477cb644f6d84
Yaroslav R <yaroslav.r@it-dimension.com>
2018-08-07 15:25:33 +0300

    Revert mistakenly added changes.

--- __main__.py
+++ __main__.py
@@ -5,14 +5,14 @@ import os.path
 import util.logger
 import webserver
 
-import routes
+import funder.routes
 
 # Python imports are silly.
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 # pylint: disable=wrong-import-position
-import swagger_specs
+import funder.swagger_specs
 # pylint: enable=wrong-import-position
 
 util.logger.setup()
 
-webserver.run(routes.BLUEPRINT, swagger_specs.CONFIG, routes.PORT)
+webserver.run(funder.routes.BLUEPRINT, funder.swagger_specs.CONFIG, funder.routes.PORT)
