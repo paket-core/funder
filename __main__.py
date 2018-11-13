d19e328279b0c04909a00ebbf97a0e110ab9c7ca
Israel Levin <israel@paket.global>
2018-11-13 14:04:28 +0200

    Prepare to use uwsgi

--- __main__.py
+++ __main__.py
@@ -1,18 +1,3 @@
 """Run the PaKeT funding server."""
-import sys
-import os.path
-
-import util.logger
-import webserver
-
-import funder.routes
-
-# Python imports are silly.
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-# pylint: disable=wrong-import-position
-import funder.swagger_specs
-# pylint: enable=wrong-import-position
-
-util.logger.setup()
-
-webserver.run(funder.routes.BLUEPRINT, funder.swagger_specs.CONFIG, funder.routes.PORT)
+import bridge
+bridge.APP.run('0.0.0.0', bridge.routes.PORT, bridge.webserver.validation.DEBUG)
